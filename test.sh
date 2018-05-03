8be32e8267ca8d48d9f34cea5f90ba06fbfce9cd
mahadevops <maha@devops.com>
2018-05-03 10:04:02 +0000

    mail module added with code optimization

--- test.sh
+++ test.sh
@@ -1,3 +1,2 @@
-ansible-playbook -i hosts playbooks/site.yml --skip-tags=installation
-ansible-playbook -i hosts playbooks/site.yml --skip-tags=provisioning
+ansible-playbook -i hosts playbooks/site.yml -e "ec2_launch_tag_name=mg4567" --skip-tags=installation && ansible-playbook -i hosts playbooks/site.yml  -e "ec2_launch_tag_name=mg4567 pack_tomcat=tomcat7"  --skip-tags=provisioning
 
